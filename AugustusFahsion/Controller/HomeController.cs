0d3cf0bd9174884c6537b5210258826db71a2f2d
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2021-12-12 01:01:37 -0300

    Adicionando MDI Parent

---
+++ AugustusFahsion/Controller/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using AugustusFahsion.View;
+
+namespace AugustusFahsion.Controller
+{
+    public class HomeController
+    {
+        public static void AbrirHome(MDIParent mdi)
+        {
+            var home = new Home();
+            home.MdiParent = mdi;
+            home.Show();
+        }
+    }
+}
---
+++ AugustusFahsion/View/MDIParent.Designer.cs
@@ -0,0 +1,57 @@
+﻿
+namespace AugustusFahsion.View
+{
+    partial class MDIParent
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // MDIParent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "MDIParent";
+            this.Text = "MDIParent1";
+            this.Load += new System.EventHandler(this.MDIParent_Load);
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
+
+
