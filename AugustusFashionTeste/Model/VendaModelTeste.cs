2858e2459c08dbff40800c075fe280c8e7d16097
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2021-12-14 22:47:17 -0300

    Implementado teste unitáro para verficar se o valor limite de compra a prazo foi atingido.

---
+++ AugustusFashionTeste/Model/VendaModelTeste.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AugustusFahsion.Model;
+using AugustusFahsion.Model.ValueObjects;
+
+namespace AugustusFashionTeste.Model
+{
+    [TestClass]
+    public class VendaModelTeste
+    {
+        [TestMethod]
+        public void se_valor_limite_for_atingido_retornar_falso()
+        {
+            DinheiroModel TotalLiquido = 20;
+            var totalLiquidoOriginal = 0;
+            var resultado = VendaModel.VerificarLimiteGastoCompraAPrazoFoiAtingido(22, TotalLiquido, totalLiquidoOriginal);
+            var resultadoEsperado = false;
+
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+    }
+}
