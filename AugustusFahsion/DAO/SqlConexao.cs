29f4700789bf4050f9f37880157f5da01843656f
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2022-01-11 20:48:47 -0300

    Alterado String de conexão

--- AugustusFahsion/DAO/SqlConexao.cs
+++ AugustusFahsion/DAO/SqlConexao.cs
@@ -5,11 +5,10 @@ namespace AugustusFahsion.DAO
 {
     public class SqlConexao
     {
-        //Servidor do Estágio:
-        //const string connection = @"Data Source=DESKTOP-OEURUSN;Initial Catalog=AugustusFashionDatabase;Integrated Security=True";
-        
-        //Servidor Pessoal:
-        const string connection = @"Data Source=DESKTOP-6EG6G0U\SQLEXPRESS;Initial Catalog=AugustusFashionDataBase;Integrated Security=True";
+ 
+        //String de conexão:
+        const string connection = @"Data Source=HOME_PC;Initial" +
+            " Catalog=AugustusFashionDatabase;Integrated Security=True";
         
         public IDbConnection Connection() =>
             new SqlConnection(connection);
